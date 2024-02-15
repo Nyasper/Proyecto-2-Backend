1f98f9d5d87ad334af71ed7e5c90417f85f183cc
Nyasper <herreragonzalo39@gmail.com>
2024-02-14 22:00:25 -0300

    fixes

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,8 @@ app.get('/', () => 'Proyecto 2 Backend');
 
 async function run() {
 	try {
-		await app.listen({ port: 3000 });
+		const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+		await app.listen({ port });
 	} catch (err) {
 		app.log.error(err);
 		process.exit(1);
