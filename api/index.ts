abc170d01d465cc338ee06834ffea5b06f1a4ec5
Nyasper <herreragonzalo39@gmail.com>
2024-02-14 22:36:34 -0300

    fixes

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,7 @@
 import app from '../src';
+import { FastifyRequest, FastifyReply } from 'fastify';
 
-export default app;
+export default async (req: FastifyRequest, res: FastifyReply) => {
+	await app.ready();
+	app.server.emit('request', req, res);
+};
